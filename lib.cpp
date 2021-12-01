bc3f792fb6acde2e70c0447ff3b3e81acdb8ca8e
ClaudioPasciucco <91784159+ClaudioPasciucco@users.noreply.github.com>
2021-12-01 19:47:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+
+
+bool Primo (int i, int n){
+    if (i == n){
+        return true;
+    } else if (n==1) {
+        return false;
+    } else if (n%i==0) {
+        return false;
+    } else {
+        return Primo(i+1,n);
+            }
+}
 
